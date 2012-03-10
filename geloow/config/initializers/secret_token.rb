0d26d9c2ec23a61827a7ca2f86f3215fb622336c
Angel Rojas <angelsystem1376@gmail.com>
2012-03-10 00:08:29 -0500

    first commit

---
+++ geloow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geloow::Application.config.secret_token = '3b01eb1b194cf067019db0c042675f3797ebbccb78633661b3dd9bb1f233ce5303194f8dfb88bf4aa9c950a6f942fa59ab126b04a21b24a0493a7ffddd22d373'
